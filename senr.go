c84813a89c22db0b4b7b6e58fb7df892d4cc3ec1
SeongJae Park <sj38.park@gmail.com>
2016-03-26 11:41:04 +0900

    print out usage if no argument received
    Signed-off-by: SeongJae Park <sj38.park@gmail.com>

--- senr.go
+++ senr.go
@@ -1,7 +1,19 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+const USAGE = "senr <config file path>"
 
 func main() {
+	args := os.Args
+	if len(args) < 2 {
+		fmt.Printf("No argument.\n")
+		fmt.Printf(USAGE)
+		os.Exit(1)
+	}
+
 	fmt.Println("hello")
 }
